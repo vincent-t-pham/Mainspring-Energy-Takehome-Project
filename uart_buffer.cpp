3894c3c2afb729f8ab07465527bbebe3000f6718
vincent-t-pham <59778724+vincent-t-pham@users.noreply.github.com>
2023-01-12 10:51:37 -0800

    Added control loop, ISR, and push to queue

--- uart_buffer.cpp
+++ uart_buffer.cpp
@@ -2,6 +2,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+bool ISR14 = false;
 struct peripheralBufferStruct 
 {
     queue<uint_fast8_t> q;
@@ -12,26 +13,25 @@ struct peripheralBufferStruct
 
 struct peripheralBufferStruct peripheralBuffer;
 
-void push(peripheralBufferStruct &peripheralBuffer, uint_fast8_t in);
+void push(peripheralBufferStruct &peripheralBuffer, uint_fast8_t in);   // Push with FIFO constraint
 void printQueue(queue<uint_fast8_t> q);
+void ISR14handler();
 //void push to queue and determine size constraint?
 
 // What's in the queue? I'm going to put uint_fast8_t so it can be parsed by 2 hex digits per index
 // Why do we use queue struct vs other stuff? Queue had functionality to push pop in FIFO manner, 
 // but missed the size constraint
 
-
+//test harness (test cases)
+uint_fast8_t inp1[] = {0x12, 0x34, 0x56, 0x78, 0xA5, 0x5A, 0x9A, 0xA2};
 
 int main(){
-    for(uint_fast8_t i=10; i<15; i++)
+    for(uint_fast8_t i=0; i<8; i++)
     {
         cout << unsigned(i) << " ";
-        push(peripheralBuffer, i);
+        push(peripheralBuffer, inp1[i]);
     }
-
-    // CONTROL LOOP //
-    // How do we know when sequence is complete? O isr is called, find how many sequences by parsing entire struct
-
+    
     // ERROR HANDLING //
     cout << endl <<  "Elements: " << endl;
     printQueue(peripheralBuffer.q);
@@ -39,6 +39,22 @@ int main(){
 
     cout << endl;
 
+    // CONTROL LOOP //
+    // How do we know when sequence is complete? O isr is called, find how many sequences by parsing entire struct
+    while(1)
+    {
+        cout << "Control Loop: " << endl;
+        sleep(10);
+        if(ISR14)
+        {
+            for(uint_fast8_t i = 0; i<8; i++)
+            {
+                // if(peripheralBuffer.q[i])
+                cout << peripheralBuffer.q[i] << endl;
+            }
+            ISR14 = false;
+        }
+    }
     
     return 0;
 }
@@ -53,8 +69,16 @@ int main(){
 
 void push(peripheralBufferStruct &peripheralBuffer, uint_fast8_t in)
 {
-    if (peripheralBuffer.q.size() < 16)
+    //edge cases
     peripheralBuffer.q.push(in);
+
+    // queue size of 8 with 1 byte width or 16 with 1 nibble width..? see line 26
+    if (peripheralBuffer.q.size() == 16)
+    {
+        // temporary ISR
+        ISR14handler();
+        // clear struct function
+    }
 }
 
 // print the elements of queue
@@ -67,4 +91,10 @@ void printQueue(queue<uint_fast8_t> q)
         q.pop();
     }
     cout << endl;
+}
+
+// temporary ISRhandler
+void ISR14handler()
+{
+    ISR14 = true;
 }
